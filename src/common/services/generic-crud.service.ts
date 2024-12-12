3c59d7e8e22783774a9feef1993d4c526cc9da4b
Tejas Mahajan <tejasmahajan416@gmail.com>
2024-12-12 22:18:56 +0530

    added more methods for crud

--- src/common/services/generic-crud.service.ts
+++ src/common/services/generic-crud.service.ts
@@ -1,5 +1,5 @@
 import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
-import { Repository, DeepPartial, FindOptionsWhere, FindManyOptions, In } from 'typeorm';
+import { Repository, DeepPartial, FindOptionsWhere, FindManyOptions, In, SelectQueryBuilder, UpdateResult, DeleteResult } from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 @Injectable()
@@ -16,8 +16,14 @@ export class GenericCrudService<T> {
     return await this.repository.save(entities);
   }
 
-  async findOne(conditions: FindOptionsWhere<T>): Promise<T | null> {
-    return await this.repository.findOneBy(conditions);
+  async findOne(
+    conditions: FindOptionsWhere<T>,
+    relations?: string[]
+  ): Promise<T | null> {
+    return await this.repository.findOne({
+      where: conditions,
+      relations: relations || []
+    });
   }
 
   async findAll(options?: FindManyOptions<T>): Promise<T[]> {
@@ -33,38 +39,51 @@ export class GenericCrudService<T> {
     });
   }
 
-  async isExist(conditions: FindOptionsWhere<T>): Promise<T | null> {
-    const entity = await this.findOne(conditions);
+  async queryAll(
+    queryBuilder?: (qb: SelectQueryBuilder<T>) => SelectQueryBuilder<T>
+  ): Promise<T[]> {
+    const qb = this.repository.createQueryBuilder();
+    return await queryBuilder(qb).getMany();
+  }
+
+  async isExist(
+    conditions: FindOptionsWhere<T>,
+    relations?: string[]
+  ): Promise<T | null> {
+    const entity = await this.findOne(conditions, relations);
     if (entity) throw new ConflictException('Already exist.');
     return entity;
   }
 
-  async validate(conditions: FindOptionsWhere<T>): Promise<T | null> {
-    const entity = await this.findOne(conditions);
+  async validate(
+    conditions: FindOptionsWhere<T>,
+    relations?: string[]
+  ): Promise<T | null> {
+    const entity = await this.findOne(conditions, relations);
     if (!entity) throw new NotFoundException();
     return entity;
   }
 
-  async update(id: any, entity: QueryDeepPartialEntity<T>): Promise<T | object> {
+  async update(id: any, entity: QueryDeepPartialEntity<T>): Promise<UpdateResult> {
     return await this.repository.update(id, entity);
   }
 
-  async deleteOne(id: any): Promise<void> {
-    await this.repository.delete(id);
+  async delete(id: any): Promise<DeleteResult> {
+    return await this.repository.delete(id);
   }
 
-  async deleteAll(where?: FindOptionsWhere<T>): Promise<void> {
-    await this.repository.delete(where || {});
-  }  
-  
-  // Default soft delete
-  // async softDelete(id: any): Promise<void> {
-  //   await this.repository.softDelete(id);
+  async deleteAll(where?: FindOptionsWhere<T>): Promise<DeleteResult> {
+    return await this.repository.delete(where || {});
+  }
+
+  // Soft delete entities by criteria
+  // async softDelete(criteria: FindOptionsWhere<T>): Promise<UpdateResult> {
+  //   return await this.repository.softDelete(criteria);
   // }
 
   // Advanced soft delete
-  async softDelete(id: any): Promise<void> {
-    await this.repository
+  async softDelete(id: any): Promise<UpdateResult> {
+    return await this.repository
       .createQueryBuilder()
       .update()
       .set({ deletedAt: new Date(), isDeleted: true } as any)  // Casting to `any`
@@ -72,4 +91,22 @@ export class GenericCrudService<T> {
       .execute();
   }
 
+  // Restore soft-deleted entities by criteria
+  // async restore(criteria: FindOptionsWhere<T>): Promise<UpdateResult> {
+  //   return await this.repository.restore(criteria);
+  // }
+
+  async restore(id: any): Promise<UpdateResult> {
+    return await this.repository
+      .createQueryBuilder()
+      .update()
+      .set({ deletedAt: null, isDeleted: false } as any)  // Casting to `any`
+      .where("uuid = :id", { id })
+      .execute();
+  }
+
+  // Perform raw SQL query
+  async performQuery(query: string, parameters?: any[]): Promise<any> {
+    return await this.repository.query(query, parameters);
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { GenericCrudService } from 'src/common/services/generic-crud.service';
 import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,5 +11,12 @@ export class UserService extends GenericCrudService<UserEntity> {
         private readonly userRepository: Repository<UserEntity>,
     ) {
         super(userRepository);
+    }
+
+    // You can add additional methods specific to UserEntity if needed
+    async findByEmail(email: string): Promise<UserEntity | null> {
+        const user = await this.userRepository.findOne({ where: { email } });
+        if (!user) throw new NotFoundException('User not found.');
+        return user;
     }
 }
